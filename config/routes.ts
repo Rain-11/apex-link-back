e04ddbfada066e46ade7e4250069c54c29cf479f
CrazyRain <1147932729@qq.com>
2024-04-20 13:29:13 +0800

    移除不必须页面

--- config/routes.ts
+++ config/routes.ts
@@ -1,10 +1,9 @@
-export default [
+﻿export default [
   {
     path: '/user',
     layout: false,
     routes: [{ name: '登录', path: '/user/login', component: './User/Login' }],
   },
-  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
     name: '管理页',
@@ -16,6 +15,5 @@ export default [
     ],
   },
   { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
-  { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
